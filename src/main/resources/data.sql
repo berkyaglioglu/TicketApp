b5e62742a4e0eb727827c3ae1d18a4c93c688de4
berkyaglioglu <berkyaglioglu@sabanciuniv.edu>
2020-03-09 13:18:24 +0300

    minor changes

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into airport values (1,'IST');
-insert into airport values (2,'SBH');
-insert into airport values (3,'HEA');
-insert into route values (1,1,2);
-insert into airline values (1,'PGS');
--- src/test/java/com/example/demo/FinartzApplicationTests.java
+++ src/test/java/com/example/demo/FinartzApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
+import com.example.demo.dao.AirlineRepo;
+import com.example.demo.dao.AirportRepo;
+import com.example.demo.dao.FlightRepo;
+import com.example.demo.dao.RouteRepo;
+import com.example.demo.dao.TicketRepo;
+import com.example.demo.model.Airline;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = FinartzApplication.class)
+@WebAppConfiguration
 class FinartzApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+	
+	protected MockMvc mvc;
 
 }
